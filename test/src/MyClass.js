1b66a8858eb711271a2b7cfdfbfe9d99ab768a13
Raoul v. R <vanruesc@outlook.de>
2017-10-28 22:53:39 +0200

    Updated the test setup.

---
+++ test/src/MyClass.js
@@ -0,0 +1,16 @@
+/**
+ * Description of MyClass.
+ */
+
+export default class MyClass {
+
+	/**
+	 * Description of the method.
+	 *
+	 * @param {Number} param - Description of the parameter.
+	 * @return {Number} Description of the return value.
+	 */
+
+	method(param) {}
+
+}
